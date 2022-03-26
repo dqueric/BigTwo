acd3ce0ec99f78a54c669c62c613ed30bf0f013c
unknown <dqeric99@gmail.com>
2022-03-26 18:17:42 +1100

    added deck, cards, hand

---
+++ Card.py
@@ -0,0 +1,22 @@
+SUIT_VALUE = {"S": 3, "H": 2, "C": 1, "D": 0}
+RANK_VALUE = {}
+for i in range(3, 11):
+    RANK_VALUE[(str(i))] = i
+RANK_VALUE["J"] = 11
+RANK_VALUE["Q"] = 12
+RANK_VALUE["K"] = 13
+RANK_VALUE["2"] = 14
+
+class Card:
+    def __init__(self, suit :str, rank :str):
+        self.suit = suit
+        self.rank = rank
+        self.card_value = SUIT_VALUE[self.suit] + RANK_VALUE[self.rank]*4
+
+def compare_card(card_1, card_2):
+    if card_1.card_value < card_2.card_value:
+        return -1
+    elif card_1.card_value > card_2.card_value:
+        return 1
+    else:
+        return 0
---
+++ Deck.py
@@ -0,0 +1,12 @@
+import random
+from Card import Card
+class Deck:
+    def __init__(self):
+        self.deck = []
+        for suit in ["H", "C", "S", "D"]:
+            for rank in [str(i) for i in range(2, 11)] + ["J", "Q", "K"]:
+                self.deck.append(Card(suit, rank))
+        random.shuffle(self.deck)
+
+    def shuffle(self):
+        random.shuffle(self.deck)
